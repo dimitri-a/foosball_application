e1c97b356bfe24910a3438d8a7288f1460142b9f
Dimitri Couwenberg <couwenberg@gmail.com>
2017-12-17 14:06:52 +1100

    versus wins

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -4,13 +4,14 @@ import FilterLink from "./FilterLink";
 import Header from "./Header";
 import {bindActionCreators} from "redux";
 import * as actions from "../actions/index";
+import {getRate} from "../reducers/matches";
 
 class Matches extends Component {
 
     constructor(props) {
         super();
         this.props = props;
-        this.state ={selectedPlayer1:this.props.players[0].name,selectedPlayer2:this.props.players[1].name}
+        this.state = {selectedPlayer1: this.props.players[0].name, selectedPlayer2: this.props.players[1].name}
     }
 
 
@@ -19,21 +20,56 @@ class Matches extends Component {
         this.props.actions.addMatch(this.state.selectedPlayer1, this.state.selectedPlayer2, event.target.value)
         this.props.actions.addWin(event.target.value);
 
-        //todo remove
-        debugger;
-
-        this.props.actions.addLoss(event.target.value === this.state.selectedPlayer1 ? this.state.selectedPlayer2 :this.state.selectedPlayer1);
+        this.props.actions.addLoss(event.target.value === this.state.selectedPlayer1 ? this.state.selectedPlayer2 : this.state.selectedPlayer1);
     }
 
     selectPlayer = (event) => {
 
-      event.target.id ==='p1' ?
-          this.setState({selectedPlayer1:event.target.value})
-          :
-          this.setState({selectedPlayer2:event.target.value})
+        event.target.id === 'p1' ?
+            this.setState({selectedPlayer1: event.target.value})
+            :
+            this.setState({selectedPlayer2: event.target.value})
     }
 
+    versus = () => {
+        let display = '';
+        let nrWinsPlayer1=0;
+        let nrWinsPlayer2=0;
+
+//select matches 2 players
+        let result = this.props.matches.filter(match => {
+            return match.player1 === this.state.selectedPlayer1 && match.player2 === this.state.selectedPlayer2;
+        })
+
+
+console.log('result=',result);
+
+        result.forEach(
+            match => {
+                //todo remove
+                debugger;
+                if (match.winner === this.state.selectedPlayer1) {
+                    nrWinsPlayer1 = nrWinsPlayer1 + 1;
+                }
+                if (match.winner === this.state.selectedPlayer2) {
+                    nrWinsPlayer2 = nrWinsPlayer2 + 1;
+                }
+            }
+        );
+        
+      
+        console.log('nrWinsPlayer1=',nrWinsPlayer1);
+
+        console.log('nrWinsPlayer2=',nrWinsPlayer2);
+
+        display = <span>{this.state.selectedPlayer1} {nrWinsPlayer1} wins vs {this.state.selectedPlayer2} {nrWinsPlayer2} wins  </span>
+
+        return display;
+    }
+
+
     render() {
+        let display='';
         console.log('inside matches;this.props.players=', this.props.players);
 
         let options = <option></option>;
@@ -54,33 +90,40 @@ class Matches extends Component {
         let matches = this.props.matches.map(
             match =>
                 <div>
-                    {match.player1} vs {match.player2}  winner: {match.winner} date: {match.dt}
+                    winner: {match.winner} date: {match.dt}
                 </div>
         )
+
+        console.log('this.props.rate=', this.props.rate);
+
+        display = this.versus();
 
         return (<div>
 
                 <Header/>
 
-                <select id="p1" onChange={this.selectPlayer} value ={this.state.selectedPlayer1}>
+                <select id="p1" onChange={this.selectPlayer} value={this.state.selectedPlayer1}>
                     {options}
                 </select>
 
-                <button onClick={this.winner} value={this.state.selectedPlayer1}>{this.state.selectedPlayer1} wins</button>
+                <button onClick={this.winner} value={this.state.selectedPlayer1}>{this.state.selectedPlayer1} wins
+                </button>
 
 
                 <select id="p2" onChange={this.selectPlayer} value={this.state.selectedPlayer2}>
                     {options}
                 </select>
 
-                <button onClick={this.winner} value={this.state.selectedPlayer2}>{this.state.selectedPlayer2} wins</button>
+                <button onClick={this.winner} value={this.state.selectedPlayer2}>{this.state.selectedPlayer2} wins
+                </button>
 
                 <br/>
+
+                {display}
 
                 {matches}
 
 
-                {/*<button onClick={this.saveResult}>Save result</button>*/}
 
             </div>
         );
@@ -97,6 +140,7 @@ const mapStateToProps = function (state) {
 
 const mapDispatchToProps = dispatch => ({
     actions: bindActionCreators(actions, dispatch)
+
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Matches)
--- src/reducers/matches.js
+++ src/reducers/matches.js
@@ -1,10 +1,19 @@
-const matches = (state=[], action) => {
+const matches = (state = [], action) => {
     switch (action.type) {
         case 'ADD_MATCH':
-            return [...state,{player1:action.p1,player2:action.p2,winner:action.winner,dt:new Date().toString().slice(0,25)}]
+            return [...state, {
+                player1: action.p1,
+                player2: action.p2,
+                winner: action.winner,
+                dt: new Date().toString().slice(0, 25)
+            }]
         default:
             return state;
     }
 };
 
 export default matches;
+
+//
+// export const getRate = (state,p1,p2) =>
+//     state.matches.filter(match => match.player1===p1 && match.player2===p2)
